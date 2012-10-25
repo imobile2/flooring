dab69fe78475f0ba7c668c59a07d4e44e2de14a6
imobile2 <imobile2@gmail.com>
2012-10-24 18:40:09 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Flooring::Application.config.secret_token = '25f05161696433f1baebb03ab6d744c2ec138f5117d950c6bac7c0c35178dfef1d8ec9f8955242ad2d0582917a48c833420387ab4b0ab92ca4dc64c14e03d924'
